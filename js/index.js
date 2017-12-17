de8adb9c6b50884aac39339beb56f4db178fc2ad
subhadarsinisahoo <subhadarsinisahoo@gmail.com>
2017-12-17 12:53:49 +0530

    testingv9

--- js/index.js
+++ js/index.js
@@ -74,7 +74,7 @@ var app = {
 			
 			////////////database store registration //////////
 			$("#GCM_ID").html('GCM ID...'+data.registrationId);
-			alert('GCM ID...'+data.registrationId);
+			//alert('GCM ID...'+data.registrationId);
 			var dataString="regID="+data.registrationId;
 			var BaseURL = "http://pocketapp.in/unionchurch/phonegap/";
 			$.ajax({
