d6c268a875eb09bc851c7826c223173572977a8d
subhadarsinisahoo <subhadarsinisahoo@gmail.com>
2017-12-17 12:41:15 +0530

    test nayana

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@
  */ 
 var app = {
     // Application Constructor
-    initialize: function() { alert("called");
+    initialize: function() { //alert("called");
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -76,125 +76,7 @@ var app = {
 			$("#GCM_ID").html('GCM ID...'+data.registrationId);
 			alert('GCM ID...'+data.registrationId);
 			var dataString="regID="+data.registrationId;
-			var BaseURL = "https://pocketapp.in/unionchurch/phonegap/";
-			$.ajax({
-                    type: "POST",
-                    url: BaseURL+"insert_gcm_reg_id.php?",
-                    //url:"http://localhost/phonegap/database/insert.php",
-                    data: dataString,
-                    crossDomain: true,
-                    cache: false,
-                    beforeSend: function(){ $("#GCM_ID").html('Connecting...'+data.registrationId); },
-                    success: function(dataINS){
-                        if(dataINS=="ok")
-                        {}
-                        else if(dataINS=="error")
-                        {} 
-						 else if(dataINS=="duplicate")
-                        {}
-                    }
-                });
-			
-			///////////////////database input registration ////////
-        });
-
-        push.on('error', function(e) {
-            console.log("push error = " + e.message);
-        });
-
-        push.on('notification', function(data) {
-            console.log('notification event');
-			alert(data.title);
-			//$(location).attr('href', data.click_action);
-            navigator.notification.alert(
-                data.message,         // message
-                null,                 // callback
-                data.title,           // title
-                'Ok'                  // buttonName
-            );
-       });
-    }
-};
-=======
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */ 
-var app = {
-    // Application Constructor
-    initialize: function() { //alert("called");
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {  //alert("called");
-        document.addEventListener('deviceready', this.onDeviceReady, false); 
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() { //alert("34275528959");
-        console.log('Received Device Ready Event');
-        console.log('calling setup push');
-        app.setupPush();
-    },
-    setupPush: function() { 
-        console.log('calling push init');
-        var push = PushNotification.init({
-            "android": {
-                "senderID": "135371000011"
-            },
-            "browser": {},
-            "ios": {
-                "sound": true,
-                "vibration": true,
-                "badge": true
-            },
-            "windows": {}
-        });
-        console.log('after init');
-
-        push.on('registration', function(data) {
-            console.log('registration event: ' + data.registrationId);
-			
-			
-
-            var oldRegId = localStorage.getItem('registrationId');
-            if (oldRegId !== data.registrationId) {
-                // Save new registration ID
-                localStorage.setItem('registrationId', data.registrationId);
-                // Post registrationId to your app server as the value has changed
-            }
-
-            /*var parentElement = document.getElementById('registration');
-            var listeningElement = parentElement.querySelector('.waiting');
-            var receivedElement = parentElement.querySelector('.received');
-
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');*/
-			
-			////////////database store registration //////////
-			$("#GCM_ID").html('GCM ID...'+data.registrationId);
-			//alert('GCM ID...'+data.registrationId);
-			var dataString="regID="+data.registrationId;
-			var BaseURL = "https://freshboxoffice.com/phonegap/";
+			var BaseURL = "http://pocketapp.in/unionchurch/phonegap/";
 			$.ajax({
                     type: "POST",
                     url: BaseURL+"insert_gcm_reg_id.php?",
@@ -233,4 +115,3 @@ var app = {
        });
     }
 };
->>>>>>> 4974d4402284313313250cde53d545b6418877e9
